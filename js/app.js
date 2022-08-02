4a094ec2006d82950eef64e1759de94be996670b
Nate <hosiern@msn.com>
2022-08-01 22:11:49 -0400

    V1 Fully Functional Program!

--- js/app.js
+++ js/app.js
@@ -1,41 +1,128 @@
-const errorMsg = document.querySelector(".error-msg");
+const errorMsg = document.querySelector(".error-msg"); // VAR for error message
+let rgbResult = document.querySelector(".rgb-result"); // VAR for span containing output
+let rgbP = document.querySelector(".rgb-p"); // VAR for P containing output
+let hexColor = document.querySelector(".hex-color"); //VAR to for DOM object hex-color DIV
 
-// Create a VAR to modify DOM rgb-result clas
-const rgbValue = document.querySelector(".rgb-result");
+function makeHexArray() {
+  //Function to take the raw input and convert to array
+  rgbResult.textContent = ""; // Clear rgbResult from last run of converter
+  rgbP.style.visibility = "hidden";
+  hexColor.style.visibility = "hidden";
 
-// Create a VAR to modify DOM hex-color class
-const hexColor = document.querySelector(".hex-color");
+  const hexStr = document.querySelector("#hex-value").value; //VAR to contain raw input value
+  let hexArray = hexStr.split(""); //VAR to convert the string to an array of characters
+  const hashIndex = hexArray.indexOf("#"); //VAR to check the array for a # and set a value to it if found
 
-document.querySelector(".convert").preventDefault;
-
-document.querySelector(".convert").onclick = function convertRGB() {
-  //  Create variable that receives input from DOM hex-value ID
-  let hexValue = [document.querySelector("#hex-value").split("")];
-
-  // Check each character to ensure it isn't greater than f
-  for (i = 0; i < hexValue.length; i++) {
-    if (hexValue[i] > "f") {
+  for (i = 0; i < hexArray.length; i++) {
+    //Loop to determine if a character with a value higher than f was input and return error if so
+    if (hexArray[i] > "f") {
       errorMsg.style.visibility = "visible";
       document.querySelector("#hex-value").focus();
       return false;
     }
     errorMsg.style.visibility = "hidden";
   }
-  console.log(hexValue);
-};
 
-function showHexValueProper() {
-  let finalHex;
-  if (hexValue.charAt(0) != "#") {
-    finalHex = "#" + hexValue;
-  } else {
-    finalHex = hexValue;
+  if (hashIndex != -1) {
+    //Check if # is in the array and remove if so
+    hexArray.splice(hashIndex, 1);
   }
-  const setHexColor = function (e) {
-    showHexValueProper();
-    hexColor.setAttribute("visibility", "visible");
-    hexColor.style.backgroundColor = finalHex;
-  };
+
+  if (
+    //Verify array length is either 3 or 6 and return an error if not
+    hexArray.length < 3 ||
+    (hexArray.length > 3 && hexArray.length < 6) ||
+    hexArray.length > 6
+  ) {
+    errorMsg.style.visibility = "visible";
+    document.querySelector("#hex-value").focus();
+    return false;
+  }
+  errorMsg.style.visibility = "hidden";
+
+  if (hexArray.length === 3) {
+    //If array is 3 characters long, concat to make a valid full HEX
+    hexArray = hexArray.concat(hexArray);
+  }
+
+  //Call the function to set the DIV color based on HEX value
+  setColor(hexArray);
+  calcRGB(hexArray);
+}
+
+//Function to set the background color for the div
+function setColor(hexArray) {
+  //VARs to convert the array back to a valid HEX string starting with a #
+  let hexValue = hexArray.join("");
+  let setHexColor = "#" + hexValue;
+
+  //Set background color on DIV
+  hexColor.style.backgroundColor = setHexColor;
+
+  //Make DIV visible
+  hexColor.style.visibility = "visible";
+}
+
+function calcRGB(hexArray) {
+  hexArray.forEach(() => {
+    let indexA = hexArray.indexOf("a");
+    let indexB = hexArray.indexOf("b");
+    let indexC = hexArray.indexOf("c");
+    let indexD = hexArray.indexOf("d");
+    let indexE = hexArray.indexOf("e");
+    let indexF = hexArray.indexOf("f");
+
+    if (indexA != -1) {
+      hexArray[indexA] = 10;
+    }
+    if (indexB != -1) {
+      hexArray[indexB] = 11;
+    }
+    if (indexC != -1) {
+      hexArray[indexC] = 12;
+    }
+    if (indexD != -1) {
+      hexArray[indexD] = 13;
+    }
+    if (indexE != -1) {
+      hexArray[indexE] = 14;
+    }
+    if (indexF != -1) {
+      hexArray[indexF] = 15;
+    }
+  });
+
+  let r = parseFloat(hexArray[0]) * 16 + parseFloat(hexArray[1]);
+  let g = parseFloat(hexArray[2]) * 16 + parseFloat(hexArray[3]);
+  let b = parseFloat(hexArray[4]) * 16 + parseFloat(hexArray[5]);
+
+  if (r > 0 && r < 10) {
+    r = "00" + r;
+  }
+
+  if (g > 0 && g < 10) {
+    g = "00" + g;
+  }
+
+  if (b > 0 && b < 10) {
+    b = "00" + b;
+  }
+
+  if (r >= 10 && r < 100) {
+    r = "0" + r;
+  }
+
+  if (g >= 10 && g < 100) {
+    g = "0" + g;
+  }
+
+  if (b >= 10 && b < 100) {
+    b = "0" + b;
+  }
+
+  let rgb = r + ", " + g + ", " + b;
+  rgbResult.append(rgb);
+  rgbP.style.visibility = "visible";
 }
 
 // 1. Check the value of each character of HEX string
